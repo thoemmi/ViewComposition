2863feee401e79eb51170fb0046acea74b10f057
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-10-06 00:51:39 +0200

    one file per class

--- ViewComposition/Renderers/IRenderer.cs
+++ ViewComposition/Renderers/IRenderer.cs
@@ -6,9 +6,4 @@ namespace ViewComposition.Renderers {
     public interface IRenderer {
         RenderInfo Render(Document document);
     }
-
-    public class RenderInfo {
-        public string PartialViewName { get; set; }
-        public object Model { get; set; }
-    }
 }
---
+++ ViewComposition/Renderers/RenderInfo.cs
@@ -0,0 +1,6 @@
+﻿namespace ViewComposition.Renderers {
+    public class RenderInfo {
+        public string PartialViewName { get; set; }
+        public object Model { get; set; }
+    }
+}
