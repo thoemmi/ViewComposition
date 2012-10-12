c1f36161c6831b48b2ec0c48a2dd4eaf9fa74694
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-10-13 01:18:05 +0200

    added RavenDB

---
+++ ViewComposition/App_Start/RavenConfig.cs
@@ -0,0 +1,28 @@
+﻿using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Embedded;
+using Raven.Client.Indexes;
+using Raven.Client.MvcIntegration;
+using StructureMap;
+
+namespace ViewComposition.App_Start {
+    public static class RavenConfig {
+        private static IDocumentStore _documentStore;
+
+        public static void Initialize(IContainer container) {
+            if (_documentStore != null) {
+                return;
+            }
+
+            _documentStore = new EmbeddableDocumentStore {
+                DataDirectory = "App_Data"
+            }.Initialize();
+
+            IndexCreation.CreateIndexes(typeof (RavenConfig).Assembly, _documentStore);
+
+            RavenProfiler.InitializeFor(_documentStore);
+
+            container.Configure(x => x.For<IDocumentSession>().HybridHttpOrThreadLocalScoped().Use(() => _documentStore.OpenSession()));
+        }
+    }
+}
--- ViewComposition/Global.asax.cs
+++ ViewComposition/Global.asax.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using StructureMap;
+using ViewComposition.App_Start;
 
 namespace ViewComposition {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication {
+    public class MvcApplication : HttpApplication {
         protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
 
@@ -19,6 +19,11 @@ namespace ViewComposition {
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RavenConfig.Initialize(ObjectFactory.Container);
+        }
+
+        protected void Application_EndRequest(object sender, EventArgs e) {
+            ObjectFactory.ReleaseAndDisposeAllHttpScopedObjects();
         }
     }
 }
