490d94bf9dbf38dd5c98b11c5210fc53a0864852
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-10-13 00:56:45 +0200

    we don't need a favicon for now

--- ViewComposition/App_Start/RouteConfig.cs
+++ ViewComposition/App_Start/RouteConfig.cs
@@ -9,6 +9,7 @@ namespace ViewComposition {
     public class RouteConfig {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("favicon.ico");
 
             routes.MapRoute(
                 name: "Default",
