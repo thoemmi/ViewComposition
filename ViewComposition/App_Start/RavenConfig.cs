29aac1987b286755e85696acffa109338b6f9f29
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-10-13 02:54:37 +0200

    store some test documents in the database

--- ViewComposition/App_Start/RavenConfig.cs
+++ ViewComposition/App_Start/RavenConfig.cs
@@ -1,8 +1,11 @@
-﻿using Raven.Client;
+﻿using System.Linq;
+using Raven.Client;
+using Raven.Client.Linq;
 using Raven.Client.Embedded;
 using Raven.Client.Indexes;
 using Raven.Client.MvcIntegration;
 using StructureMap;
+using ViewComposition.Entities;
 
 namespace ViewComposition.App_Start {
     public static class RavenConfig {
@@ -14,12 +17,38 @@ namespace ViewComposition.App_Start {
             }
 
             _documentStore = new EmbeddableDocumentStore {
-                DataDirectory = "App_Data"
+                DataDirectory = "App_Data",
+                UseEmbeddedHttpServer = true
             }.Initialize();
 
             IndexCreation.CreateIndexes(typeof (RavenConfig).Assembly, _documentStore);
 
             RavenProfiler.InitializeFor(_documentStore);
+
+            using (var session = _documentStore.OpenSession()) {
+                RavenQueryStatistics stats;
+                session.Query<Document>().Statistics(out stats).Take(0).ToList();
+                if (stats.TotalResults == 0) {
+                    // we need to create some documents
+                    session.Store(new Document {
+                        Path = string.Empty,
+                        Title = "Home page",
+                        Body = "<p>Welcome to this site. Go and see <a href=\"/blog\">the blog</a>.</p><p><a href=\"/about\">here</a> is the about page.</p>"
+                    });
+                    session.Store(new Document {
+                        Path = "blog",
+                        Title = "Blog",
+                        Body = "This is my blog."
+                    });
+                    session.Store(new Document {
+                        Path = "about",
+                        Title = "About",
+                        Body = "This is about this site."
+                    });
+
+                    session.SaveChanges();
+                }
+            }
 
             container.Configure(x => x.For<IDocumentSession>().HybridHttpOrThreadLocalScoped().Use(() => _documentStore.OpenSession()));
         }
