2128338ac3ef261955932474b473d6e48a2e0d61
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-10-18 15:09:32 +0200

    added Id and ParentId to documents

--- ViewComposition/Entities/Document.cs
+++ ViewComposition/Entities/Document.cs
@@ -1,5 +1,7 @@
 ﻿namespace ViewComposition.Entities {
     public class Document : IDocument {
+        public string Id { get; set; }
+        public string ParentId { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
         public string Path { get; set; }
--- ViewComposition/Entities/IDocument.cs
+++ ViewComposition/Entities/IDocument.cs
@@ -1,5 +1,7 @@
 ﻿namespace ViewComposition.Entities {
     public interface IDocument {
+        string Id { get; set; }
+        string ParentId { get; set; }
         string Title { get; set; }
         string Body { get; set; }
         string Path { get; set; }
