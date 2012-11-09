5da2609fcc73d6cf6760de67bed2d89e24132db5
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-11-09 18:58:09 +0100

    started with routing tests

---
+++ ViewComposition.Tests/MvcContrib/Assert.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace ViewComposition.Tests.MvcContrib {
+    public static class Assert {
+        public static void ShouldNotBeNull(this object Actual, string message) {
+            if (Actual == null) {
+                throw new AssertionException(message);
+            }
+        }
+        public static void ShouldEqual(this object actual, object expected, string message) {
+            if (actual == null && expected == null) {
+                return;
+            }
+            if (!actual.Equals(expected)) {
+                throw new AssertionException(message);
+            }
+        }
+    }
+}
---
+++ ViewComposition.Tests/RouteTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Routing;
+using NUnit.Framework;
+using ViewComposition.Controllers;
+using ViewComposition.Tests.MvcContrib;
+
+namespace ViewComposition.Tests {
+    [TestFixture]
+    public class RouteTests {
+        [TestFixtureSetUp]
+        public void Setup() {
+            var routes = RouteTable.Routes;
+            routes.Clear();
+            RouteConfig.RegisterRoutes(routes);
+        }
+
+        [Test]
+        public void HomeIndex() {
+            "~/home".ShouldMapTo<PageController>(action => action.Index("home"));
+        }
+    }
+}
